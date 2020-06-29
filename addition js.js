04aa9c115fa49e70acabfe6d8e35b0f99cbcd40a
GallonSchimmer <alejandrogallon@gmail.com>
2020-06-29 11:03:03 +0200

    Add files via upload

---
+++ addition js.js
@@ -0,0 +1,22 @@
+// Return the Next Number from the Integer Passed
+// Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+// Examples
+// addition(0) ➞ 1
+
+// addition(9) ➞ 10
+
+// addition(-3) ➞ -2
+// Notes
+// Don't forget to return the result.
+// If you get stuck on a challenge, find help in the Resources tab.
+// If you're really stuck, unlock solutions in the Solutions tab.
+function addition(num) {
+  num++
+  return num++
+};
+// el challenge fué correcto pero sale undefined en el console
+// en el console despues de la función hay que escribir addition(3); por ejemplo
+// o cualquier valor como addition(9834); y da la respuesta.
+// se puede en el console decirle alert(addition(98245)); y aparece en alert el
+// valor.
